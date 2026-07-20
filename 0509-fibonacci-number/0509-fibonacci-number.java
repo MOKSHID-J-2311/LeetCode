e13415fc1e3acd369c56d561384f50cfa2602ce6
MOKSHID-J-2311 <xamokshid765@gmail.com>
2026-07-20 15:48:31 +0530

    Time: 0 ms (100%), Space: 42 MB (70.17%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        if(n==0||n==1){
+            return n;
+        }
+        int p1=1;int p2=0;
+        for(int i=2;i<=n;i++){
+            int c=p1+p2;
+            p2=p1;
+            p1=c;
+        }
+        return p1;
     }
 }
